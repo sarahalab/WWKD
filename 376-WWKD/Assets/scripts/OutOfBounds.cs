e9043d68da71be3b7608dc297dd5acea3298aaa6
sarahalab <susuhalabi22@gmail.com>
2024-01-20 15:29:07 -0500

    Import assets

---
+++ 376-WWKD/Assets/scripts/OutOfBounds.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OutOfBounds : MonoBehaviour
+{
+    [SerializeField] private float boundary = -5.0f; // the value we will use for Y
+    [SerializeField] private float fallDamage;
+    private Vector3 initialPosition;
+
+    private Health playerHealth;
+    private SpikeDamage spikeDamage;
+    [SerializeField] private AudioSource drownSoundEffect;
+
+    private void Start()
+    {
+        initialPosition = transform.position;
+        playerHealth = GetComponent<Health>();
+        spikeDamage = FindObjectOfType<SpikeDamage>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+            CheckOffMap();
+    }
+
+    
+    private void CheckOffMap()
+    {
+        if (transform.position.y < boundary)
+        {
+            //drownSoundEffect.Play();
+            playerHealth.TakeDamage(fallDamage);
+            StartCoroutine(spikeDamage.DelayedRespawn());
+        }
+    }
+}

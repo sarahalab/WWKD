e5885aa04113e13a1a1102b556bfd8690321e6df
dernah <144572300+dernah@users.noreply.github.com>
2024-03-11 06:21:16 -0400

    Menu stuff

---
+++ 376-WWKD/Assets/OptionsMenu.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public GameObject MainMenuUI;
+    public GameObject optionMenuUI;
+
+    public AudioMixer audioMixer;
+    public TMP_Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+    private void Start()
+        
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height + " @" + resolutions[i].refreshRateRatio + "hz";
+            options.Add(option);
+
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        List<TMP_Dropdown.OptionData> dropdownOptions = new List<TMP_Dropdown.OptionData>();
+        foreach (string option in options)
+        {
+            TMP_Dropdown.OptionData newOption = new TMP_Dropdown.OptionData(option);
+            dropdownOptions.Add(newOption);
+        }
+
+        resolutionDropdown.AddOptions(dropdownOptions);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    public void SetVolume(float volume)
+    {
+        Debug.Log(volume);
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    } 
+    public void SaveOptions()
+    {
+        MainMenuUI.SetActive(true);
+        optionMenuUI.SetActive(false);
+    }
+}
--- 376-WWKD/Assets/scripts/MainMenu.cs
+++ 376-WWKD/Assets/scripts/MainMenu.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
-public class MainMenu : MonoBehaviour
+public class StartMenu : MonoBehaviour
 {
+    public GameObject MainMenuUI;
+    public GameObject optionMenuUI;
+
+    public void OptionsMenu()
+    {
+        MainMenuUI.SetActive(false);
+        optionMenuUI.SetActive(true);
+    }
 
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }

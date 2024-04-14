e220ec0474b78203f9d49d81c5a27bbe12696ce4
dernah <144572300+dernah@users.noreply.github.com>
2024-04-14 00:13:41 -0400

    Disabled Playercontroller when winning

--- 376-WWKD/Assets/scripts/FinishLine.cs
+++ 376-WWKD/Assets/scripts/FinishLine.cs
@@ -7,11 +7,14 @@ public class FinishLine : MonoBehaviour
 {
 
     public GameManager gameManager;
+    public PlayerController playerController;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
             // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+            playerController.CanControl = false;
 
             gameManager.CompleteLevel();
         }
